1ec1db4984657019ae3939a1fad538db3701d1fd
Phu Nguyen <nguyenducphu@gmail.com>
2012-06-14 00:07:03 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Banhmi::Application.config.secret_token = '11149ef606b8e566064ef8f97b63326930df9869cd092c4d5e68a413878deda8fdc2f597644c2ac00e1048b05464f33d3d1a7140bb3bc74efe42652dc7423180'
